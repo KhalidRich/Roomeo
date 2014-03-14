887eec57cd4f0d3a2c132f50e58fa3b1962e2d90
kugurst <ma2799@gmail.com>
2014-03-14 04:46:59 -0400

    Changed the index for User to uname.
    email used to be the index, which inforced uniqueness, hence the issues (look under mapping).

--- roomeo_dev/grails-app/domain/roomeo_dev/User.groovy
+++ roomeo_dev/grails-app/domain/roomeo_dev/User.groovy
@@ -150,12 +150,12 @@ class User {
 	}
 	
 	static mapping = {
-		email index:true, indexAttributes: [unique:true, dropDups:true]
+		uname index:true, indexAttributes: [unique:true, dropDups:true]
 	}
 	static embedded = ['address', 'personality', 'attributes']
 	static constraints = {
 		email email: true, nullable: false, validator: { val -> val.equals("") || val.endsWith(".edu") }
-		uname validator: { val, obj -> !(val == null && obj.email == null) }, unique: true
+		uname validator: { val, obj -> !(val == null && obj.email == null) }
 		password nullable: false, unique: true
 		
 		attributes nullable: false
