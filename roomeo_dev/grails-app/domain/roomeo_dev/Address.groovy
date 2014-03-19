8a90b12aba18df2bcf6c30fc4ec9ffdc01351e3c
kugurst <ma2799@gmail.com>
2014-03-19 19:43:11 -0400

    Finished the rudimentary address getting routine.
    Needs to be more robust against null, and more robust for different addresses.

--- roomeo_dev/grails-app/domain/roomeo_dev/Address.groovy
+++ roomeo_dev/grails-app/domain/roomeo_dev/Address.groovy
@@ -1,11 +1,31 @@
 package roomeo_dev
 
+import net.sourceforge.jgeocoder.us.AddressParser
+import net.sourceforge.jgeocoder.us.AddressStandardizer
+import net.sourceforge.jgeocoder.AddressComponent
+
 class Address {
 	String streetAddress
 	String city
 	String state
 	String country
 	Integer zipCode
+	
+	public static Address getAddressFromString(String addressString)
+	{
+	    // Parse the address into a mpa
+	    def addressMap = AddressParser.parseAddress(addressString)
+	    addressMap = AddressStandardizer.normalizeParsedAddress(addressMap)
+	    // Make an address and detach it
+	    def address = new Address()
+	    
+	    address.streetAddress = addressMap[AddressComponent.NUMBER] + " " + addressMap[AddressComponent.STREET]
+	    address.city = addressMap[AddressComponent.CITY]
+	    address.state = addressMap[AddressComponent.STATE]
+	    address.zipCode = Integer.parseInt(addressMap[AddressComponent.ZIP])
+	    println addressMap
+	    return address
+	}
 	
 	static belongsTo = [user: User]
     static constraints = {
